1875741494d53ed4108fed3dac8965d68594052d
Ali-eng94 <alihaci722@gmail.com>
2026-01-26 08:48:52 +0100

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,29 @@
+print("Hallo Ali Haji")
+print(55)
+print("Ali",end=" ") # with end="" the first name and the second name should be on the same line
+print("Haji")
+
+#Variables
+
+# 1:String Variable
+
+first_name = "ALi"
+last_name = "Haji"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# 2:integer Variable
+
+age = 32
+print("My age is " + str(age)) #it can only be read with this str()
+
+# 3:float Variable
+
+height = 180.5
+print("My height is " + str(height))
+
+# 4: Boolean Variable
+
+isHuman = True
+
+print(isHuman)
