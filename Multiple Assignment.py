440370d35b2bf841d2af845240f45f33eda91507
Ali-eng94 <alihaci722@gmail.com>
2026-02-21 00:57:34 +0100

    Add files via upload

---
+++ Multiple Assignment.py
@@ -0,0 +1,21 @@
+import math
+
+pi = 3.14
+
+print(round(3.14))              # Rounds to the nearest whole number
+print(math.ceil(3.14))          # Always rounds up
+print(math.floor(3.14))         # Always rounds down
+print(abs(pi))                  # Absolute value of pi
+print(pow(5,2))                 # 5 to the power of 2
+print(math.sqrt(25))            # Square root of 25
+print(max(2,5,9))               # Largest number
+
+
+#output :
+3
+4
+3
+3.14
+25
+5.0
+9
