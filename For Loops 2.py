f4ccd6e64b566ea0371fed862fdad0f4827dc87d
Ali-eng94 <alihaci722@gmail.com>
2026-02-21 01:59:08 +0100

    Add files via upload

---
+++ For Loops 2.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = 0
+for i in range(num1,num2+1):
+    result = result +i
+print(result)
+
+#output:
+#Enter the first number: 1
+#Enter the second number: 100
+#5050
