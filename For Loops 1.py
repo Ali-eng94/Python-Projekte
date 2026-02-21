e41cdb150cdaf5bd85adab3a342abe816ec1878b
Ali-eng94 <alihaci722@gmail.com>
2026-02-21 01:54:19 +0100

    Add files via upload

---
+++ For Loops 1.py
@@ -0,0 +1,24 @@
+for i in range(1,10,1):
+    print("Hello World")
+#output:
+#Hello World
+#Hello World
+#Hello World
+#Hello World
+#Hello World
+#Hello World
+#Hello World
+#Hello World
+#Hello World
+for i in range(1,10,1):
+    print(i)
+#output:
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
