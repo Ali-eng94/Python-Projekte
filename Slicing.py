4129b051ce43a1c10883ea78e48bcab61a11b04a
Ali-eng94 <alihaci722@gmail.com>
2026-02-21 01:04:23 +0100

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,17 @@
+#slicing
+#[start:stop:step]
+
+name = "ALi Haji"
+first_name = name[0:3:1]
+print(first_name)
+
+
+website = "https://www.alihaji.ai"
+print(website[2:20:1])
+website1 = "https://www.alihaji.ai"
+print(website1[8:20:1])
+
+#output:
+#ALi
+#tps://www.alihaji.
+#www.alihaji.
