8a7eb57b06a90541bfab8e8215d5bf1e55cd1f22
Ali-eng94 <alihaci722@gmail.com>
2026-02-23 23:18:56 +0100

    Add files via upload

---
+++ Index Operator.py
@@ -0,0 +1,11 @@
+name = "ALi Haji"
+
+first_name = name[0:3]
+last_name = name[3:8]
+
+print("The first name is " + first_name)
+print("The last name is " + last_name)
+
+#output :
+#The first name is ALi
+#The last name is  Haji
