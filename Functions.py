c32be33ede817301aa45995cc4877c677dcc3189
Ali-eng94 <alihaci722@gmail.com>
2026-02-23 23:30:20 +0100

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,15 @@
+def sayHi():
+    print("Hello Ali")
+
+sayHi()
+
+
+def myname(first_name, last_name,age):
+    print("Hello I'am " + first_name + " " + last_name + " My age is " + str(age))
+
+myname("Ali","Haji",32)
+
+
+#output:
+#Hello Ali
+#Hello I'am Ali Haji My age is 32
