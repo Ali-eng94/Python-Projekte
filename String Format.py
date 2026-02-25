4039aee89b20090dfefaefaa2c50e93d83bbea45
Ali-eng94 <alihaci722@gmail.com>
2026-02-25 01:20:10 +0100

    Add files via upload

---
+++ String Format.py
@@ -0,0 +1,9 @@
+
+print("{1} is the best Company in {0}".format("Germany","Youbee.ai"))
+    # output : Youbee.ai is the best Company in Germany
+
+company = "Youbee.ai"
+country = "germany"
+
+print("{1} is the best Company in {0}".format(country,company))
+    # output : Youbee.ai is the best Company in Germany
